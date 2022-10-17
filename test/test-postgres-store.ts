692c4ed83089b78ae5d539c20620c1eaac403cd8
Jan Kirchner <jan.kirchner@extern.wtl.de>
2022-10-17 21:58:10 +0000

    use env variables in test setup

--- test/test-postgres-store.ts
+++ test/test-postgres-store.ts
@@ -7,11 +7,11 @@ export class TestPostgresStore {
   static create(): TestPostgresStore {
     return new TestPostgresStore(
       new PostgresStore({
-        database: "postgres",
-        username: "postgres",
-        password: "postgres",
-        host: "host.docker.internal",
-        port: 5432,
+        database: process.env.DB_DATABASE!,
+        username: process.env.DB_USER!,
+        password: process.env.DB_PASSWORD!,
+        host: process.env.DB_HOST!,
+        port: Number.parseInt(process.env.DB_PORT!),
       })
     );
   }
